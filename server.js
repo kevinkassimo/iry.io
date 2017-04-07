d81101428b5454d36f9159a220cc19feb8fc175c
kevinkassimo <kevinkassimo@gmail.com>
2017-04-06 21:59:03 -0700

    Adding more dependencies

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash'); // Used for flash message if login failure encountered
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var exphbs = require('')
+
+var app = express();
+app.use('view engine')
